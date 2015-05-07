bb42a0f5ac71c988f462f5fd0824dd6fd21dfcb9
Ron Ellis <rkeiii@gmail.com>
2015-05-07 14:25:06 -0700

    various updates

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'rone@benetech.org'
 license          'All rights reserved'
 description      'Bookshare wrapper of openldap cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.11'
+version          '0.2.0'
 
-depends 'openldap', '~> 1.12.10'
+depends 'openldap', '~> 2.2.0'
